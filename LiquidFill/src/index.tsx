62453c2c217c4ed51a7e650e41075ae36be039a0
MohamedElPrince <mohamed.elprince@incorta.com>
2021-09-28 18:56:51 +0200

    step 2

--- LiquidFill/src/index.tsx
+++ LiquidFill/src/index.tsx
@@ -40,18 +40,35 @@ export default LiquidFill;
 
 //copied code from http://jsfiddle.net/f975pnta/
 
-$(document).ready(function () {
-  function uuid() {
-    var buf = new Uint32Array(4);
-    window.crypto.getRandomValues(buf);
-    var idx = -1;
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-      idx++;
-      var r = (buf[idx >> 3] >> ((idx % 8) * 4)) & 15;
-      var v = c === 'x' ? r : (r & 0x3) | 0x8;
-      return v.toString(16);
-    });
-  }
+function LiquidFillGaugeComponent({
+  currentValue = 50,
+  minValue = 0,
+  maxValue = 100,
+  isPercent = true
+}) {
+  const ref = useRef();
+
+  const chartMinValue = isPercent ? 0 : minValue;
+  const chartMaxValue = isPercent ? 100 : maxValue;
+  const chartValue = isPercent
+    ? ((currentValue - minValue) / (maxValue - minValue)) * 100
+    : currentValue;
+
+  useEffect(() => {
+    ref.current.innerHTML = '';
+
+    function uuid() {
+      var buf = new Uint32Array(4);
+      window.crypto.getRandomValues(buf);
+      var idx = -1;
+      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        idx++;
+        var r = (buf[idx >> 3] >> ((idx % 8) * 4)) & 15;
+        var v = c === 'x' ? r : (r & 0x3) | 0x8;
+        return v.toString(16);
+      });
+    }
+
 
   function liquidFillGaugeDefaultSettings() {
     return {
