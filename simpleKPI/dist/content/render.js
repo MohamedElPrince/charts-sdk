f10f7dd8d90f93858ac2c3c4636dd844ebc7ac2b
MohamedElPrince <mohamed.elprince@incorta.com>
2021-09-30 18:11:20 +0200

    add Group KPIs

--- simpleKPI/dist/content/render.js
+++ simpleKPI/dist/content/render.js
@@ -1,11 +1,27 @@
 var React  = window.React;
 
-var NewComponent = function NewComponent(props) {
-  console.log(props.insight.data.data[0][0]);
+//// @ts-nocheck
+
+var SimpleKPI = function SimpleKPI(_ref) {
+  var _ref$insight = _ref.insight,
+      data = _ref$insight.data;
+  console.log(data.data);
+  var formattedMeasures = data.data.map(function (col, index) {
+    console.log(col);
+    var dim = col[0],
+        hamada = col[1];
+    return {
+      row: dim.value,
+      value: +hamada.value
+    };
+  });
+  console.log("formatted measure", formattedMeasures);
   return /*#__PURE__*/React.createElement("div", {
-    className: "test"
-  }, /*#__PURE__*/React.createElement("h1", null, props.insight.data.data[0][0].value));
+    className: "SimpleKPI__wrapper"
+  }, formattedMeasures.map(function (response) {
+    return /*#__PURE__*/React.createElement("div", null, " ", /*#__PURE__*/React.createElement("strong", null, response.row, ":"), " ", response.value);
+  }));
 };
 
-export { NewComponent as default };
+export { SimpleKPI as default };
 //# sourceMappingURL=render.modern.js.map
--- simpleKPI/src/index.tsx
+++ simpleKPI/src/index.tsx
@@ -1,16 +1,36 @@
+//// @ts-nocheck
+
 import React from 'react';
 import { ComponentProps } from '@incorta-org/component-sdk';
 import './styles.less';
 
-const NewComponent = (props: ComponentProps) => {
-  console.log(props.insight.data.data[0][0]);
+const SimpleKPI = ({insight: { data, context } }: ComponentProps) => {
+  const insightData = data.data;
+  const [aggregationData] = insightData;
+
+  console.log(data.data);
+
+  const formattedMeasures = data.data.map((col, index) => {
+    console.log(col);
+
+    let [dim,hamada] = col;
+    return {
+      row: dim.value,
+      value: +hamada.value
+    };
+  });
+console.log("formatted measure", formattedMeasures);
+
   return (
-    <div className="test">
-      <h1>
-        {props.insight.data.data[0][0].value}
-        </h1>
+    <div className="SimpleKPI__wrapper">
+      {formattedMeasures.map(response => {
+        return (
+          <div> <strong>{response.row}:</strong> {response.value}
+          </div>
+        );
+      })}
     </div>
   );
 };
 
-export default NewComponent;
+export default SimpleKPI;
